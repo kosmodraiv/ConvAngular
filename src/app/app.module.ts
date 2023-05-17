b47c05e340ff159ae1b609052546010eeb1c19c2
kosmodraiv <60289387+kosmodraiv@users.noreply.github.com>
2023-05-17 23:35:56 +0300

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
 import { MainModule } from './main/main.module'
@@ -12,7 +11,6 @@ import { MainModule } from './main/main.module'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HeaderModule,
     MainModule
   ],
